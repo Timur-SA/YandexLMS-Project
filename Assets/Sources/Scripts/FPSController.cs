297e489b7c390feb0a28d8f39c0a913a5c4ff280
sikochergin <148399930+sikochergin@users.noreply.github.com>
2024-04-12 02:28:47 +0300

    commit_6
    систему начала игры

--- Assets/Sources/Scripts/FPSController.cs
+++ Assets/Sources/Scripts/FPSController.cs
@@ -10,6 +10,7 @@ public class FPSController : MonoBehaviour
     [SerializeField] private float sens = 3.0f;
     [SerializeField] private Transform _camtrans;
     [SerializeField] private GameObject _textStart;
+    [SerializeField] private GameObject _levelManager;
     [SerializeField] private Animator _gatesOpen;
 
 
@@ -84,7 +85,7 @@ public class FPSController : MonoBehaviour
             {
                 GameManager.InstanceGM.GameOn = true;
                 _textStart.SetActive(false);
-                _gatesOpen.SetTrigger("Open"); 
+                _levelManager.SetActive(true);
             }
         }
     }
---
+++ Assets/Sources/Scripts/LevelManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private Level _lvl;
+    [SerializeField] private TMP_Text _timer;
+    [SerializeField] private Animator _gatesOpen;
+
+
+    private int _secNow;
+    private float _timeNow = 0f;
+    private float _timeLeft;
+    public bool IsPlaying = true;
+    public bool LevelDone = false;
+    public int EnemiesCount = 0;
+
+    void Start()
+    {
+        _timeLeft = _lvl.TimeToLive;
+        _secNow = _lvl.TimeToLive;
+        _timer.text = _secNow.ToString();
+    }
+
+    void Update()
+    {
+        _timeLeft -= Time.deltaTime;
+        _timeNow += Time.deltaTime;
+        if (IsPlaying)
+        { 
+            if (_timeNow > 1)
+            {
+                _timeNow = 0f;
+                _secNow--;
+                if (_secNow > 59)
+                {
+                    _timer.text = (_secNow / 60).ToString() + ":" + (_secNow % 60).ToString();
+                }
+                else
+                {
+                    _timer.text = _secNow.ToString();
+                }
+            }
+            if (_secNow <= 0)
+            {
+                IsPlaying = false;
+                LevelDone = true;
+                _timer.text = "";
+            }
+        }   
+        
+
+        if (LevelDone && EnemiesCount == 0) 
+        {
+            _gatesOpen.SetTrigger("Open");
+            LevelDone = false;
+        }
+        
+    }
+}
